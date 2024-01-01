6af4cda879469870031673859744c2ed34649a25
Vinay Govekar <vinaygovekar24@gmail.com>
2024-01-01 20:27:03 +0530

    * Dynamic courses object
    * Made the courses object dynamic so that n numbers of courses along with their details are rendered using reusable component

--- part1/src/components/Course.jsx
+++ part1/src/components/Course.jsx
@@ -1,11 +1,19 @@
 import Content from "./Content"
 import Header from "./Header"
 
-const Course = ({course}) =>{
+const Course = ({courses}) =>{
     return(
         <>
-        <Header title={course.name}/>
-        <Content parts={course.parts}/>
+        <Header title={"Web Development Curriculam"}/>
+        {
+            courses.map(course=><div key={course.id}>
+                <h2>{course.name}</h2>
+                <Content parts={course.parts}/>
+                <h4>Total: {course.parts.map(part=>part.exercises).reduce((s,p)=>s+p)}</h4>
+                </div>
+                )
+        }
+        
         </>
     )
 }
